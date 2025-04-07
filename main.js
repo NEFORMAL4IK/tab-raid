990450f1922413a89bdf2068847f717360732b23
NEFORMAL4IK <goblinesy@gmail.com>
2025-04-07 22:08:56 +0300

    connect js and add secret button

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 'use strict'
+const btn = document.querySelector('input')
+console.log(btn);
+btn.addEventListener("click", e=>{
+    const rem = document.querySelector('.wrapper')
+    rem.remove()
+})
+btn.addEventListener("click", e=>{
+    const rem = document.querySelector('div.wrapper');
+    const cr = document.createElement("div");
+    cr.classList.add('GlavTxt')
+    cr.textContent = 'Всё нету саитаэ'
+    document.body.append(cr)
+    console.log(rem);
+})
