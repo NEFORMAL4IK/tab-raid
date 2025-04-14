4e898a7846937319630120d01f5c378097a55948
NEFORMAL4IK <goblinesy@gmail.com>
2025-04-14 15:02:47 +0300

    add calculator and add bueautiful code

--- main.js
+++ main.js
@@ -12,4 +12,18 @@ btn.addEventListener("click", e=>{
     cr.textContent = 'Всё нету саитаэ'
     document.body.append(cr)
     console.log(rem);
-})
+}) 
+
+const dcalc = document.querySelector('.calc')
+const sel = document.querySelectorAll('.top')
+let count = 0;
+const calc = document.createElement('p');
+calc.textContent = count; 
+dcalc.append(calc);
+
+for(let i = 0; i < sel.length; i++){
+    sel[i].addEventListener('click', e => {
+        count++;
+        calc.textContent = count;
+    })
+}//     дефолт калькулятор
